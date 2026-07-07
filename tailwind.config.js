db79bc5383e121640b754925bb6fe4750b366c10
Santiago Reyes Chávez <sa.reycha@gmail.com>
2026-07-06 23:33:32 -0600

    Safelist max-w-[1440px] and max-w-[300px] in Tailwind config
    Tailwind was purging these arbitrary-value classes because the scanner
    sometimes misses bracket values through responsive prefixes (lg:, sm:).
    Adding them to safelist guarantees they are always included in the
    compiled CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.js'],
+  safelist: [
+    'max-w-[1440px]',
+    'max-w-[300px]',
+    'lg:max-w-[1440px]',
+    'sm:max-w-none',
+  ],
   theme: {
     extend: {
       colors: {
